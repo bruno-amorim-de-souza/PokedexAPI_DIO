3c2530275a732951b6dcd181dd447d9b2d2d0d28
Bruno Amorim <0brunoamorim0@gmail.com>
2023-09-17 18:22:16 -0300

    update do projeto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,9 +5,9 @@ const pokeApi = {}
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
     pokemon.name = pokeDetail.name;
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
 
-    const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
     pokemon.types = types
     pokemon.type = type
@@ -21,10 +21,9 @@ pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
             .then((response) => response.json())
             .then(convertPokeApiDetailToPokemon)
-
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = () => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url) 
         .then((response) => response.json())
